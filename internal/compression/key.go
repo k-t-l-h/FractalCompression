5edf9118cca4d33ca19012c2cdfd53683642d053
k-t-l-h <laciedreamer@gmail.com>
2020-10-23 16:58:31 +0300

    hash function added

---
+++ internal/compression/key.go
@@ -0,0 +1,14 @@
+package compression
+
+type Key struct {
+	//название хеша для использования в запросах
+	Name string
+	//тип ключа
+	Type string
+	//длина ключа
+	Len float64
+	//встроенный в бд хеш или нет
+	Users bool
+	//для не-встроенных: текст скрипта создания
+	Script string
+}
