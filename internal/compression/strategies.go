1868ffeef46e867e1022f71450adfd1a4803ff5c
k-t-l-h <laciedreamer@gmail.com>
2020-11-12 01:16:52 +0300

    first v

--- internal/compression/strategies.go
+++ internal/compression/strategies.go
@@ -2,33 +2,42 @@ package compression
 
 import (
 	combinations "github.com/mxschmitt/golang-combinations"
-	"math"
+	"log"
 )
 
 func (t *Table) BruteForceStrategy() error {
 	//список всех имен
-	names := []string{"A", "B", "C"}
+	var names []string
 	//получение списка всех имен
 	for _, i := range t.Compressible {
 		names = append(names, t.Columns[i].Name)
 	}
-
+	log.Print(names)
 	all := combinations.All(names)
+
 	var best []string
-	var bestscore uint64
-	str := ""
-	bestscore = math.MaxUint64
+	var bestScore float64
+
+	bestScore = 1
 	for _, one := range all {
+		str := ""
 		for _, name := range one {
-			str += name + ", "
+			str = str + name + ", "
 		}
 		str = str[:len(str)-2]
-		v, _ := t.Database.GetUniqueValues(t.Name, str)
-		if v < bestscore {
-			best = one
+
+		if len(one) != 1 || len(one) != len(t.Compressible) {
+			v, _ := t.Database.GetUniqueValues(t.Name, str)
+
+			values := (float64(t.Columns[0].Values) - float64(v)) / float64(t.Columns[0].Values)
+			if values < bestScore {
+				best = one
+			}
 		}
 	}
 
+
+	//TODO: один проход
 	for _, b := range best {
 		for i, _ := range t.Columns {
 			if b == t.Columns[i].Name {
@@ -36,6 +45,5 @@ func (t *Table) BruteForceStrategy() error {
 			}
 		}
 	}
-
 	return nil
 }
